e487352418af0d1ab8996632f8500a900459837e
Antonio Piatti Fadda <antonio.piattifadda@gmail.com>
2023-09-09 23:16:09 -0300

    customer service findOne

--- api/routes/customers.router.js
+++ api/routes/customers.router.js
@@ -11,7 +11,7 @@ const {
 const router = express.Router();
 const service = new CustomerService();
 
-router.get('/',  async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     res.json(await service.find());
   } catch (error) {
@@ -19,7 +19,16 @@ router.get('/',  async (req, res, next) => {
   }
 });
 
-router.post('/',
+router.get('/:id', async (req, res, next) => {
+  try {
+    res.json(await service.findOne());
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post(
+  '/',
   validationHandler(createCustomerSchema, 'body'),
   async (req, res, next) => {
     try {
@@ -31,7 +40,8 @@ router.post('/',
   }
 );
 
-router.patch('/:id',
+router.patch(
+  '/:id',
   validationHandler(getCustomerSchema, 'params'),
   validationHandler(updateCustomerSchema, 'body'),
   async (req, res, next) => {
@@ -45,7 +55,8 @@ router.patch('/:id',
   }
 );
 
-router.delete('/:id',
+router.delete(
+  '/:id',
   validationHandler(getCustomerSchema, 'params'),
   async (req, res, next) => {
     try {
