0fde7064e640859320ea1ff8d2cc5285250b56bd
Antonio Piatti <antonio.piattifadda@gmail.com>
2023-12-13 20:36:07 -0300

    deleted query on prices

--- api/schemas/product.schema.js
+++ api/schemas/product.schema.js
@@ -7,12 +7,8 @@ const description = Joi.string().min(10);
 const image = Joi.string().uri();
 const categoryId = Joi.number().integer();
 
-const price_min = Joi.number().integer();
-const price_max = Joi.number().integer();
 const unitPrices = Joi.array().items(createUnitPriceSchemaWithProducts);
 
-const limit = Joi.number().integer();
-const offset = Joi.number().integer();
 const product = Joi.object({
   name: name.required(),
   description: description.required(),
@@ -36,19 +32,8 @@ const getProductSchema = Joi.object({
   id: id.required(),
 });
 
-const queryProductSchema = Joi.object({
-  limit,
-  offset,
-  price_min,
-  price_max: Joi.when('price_min', {
-    is: Joi.exist(),
-    then: price_max.required(),
-  }),
-});
-
 module.exports = {
   createProductSchema,
   updateProductSchema,
   getProductSchema,
-  queryProductSchema,
 };
